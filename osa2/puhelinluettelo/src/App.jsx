b4719f06c85b11a80840f10f7c52e01672c9c98a
Tomi Halko <tomi.halko@gmail.com>
2023-08-28 10:39:08 +0300

    Osan 2, tehtävä 2.6

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    //console.log(newName)
+    //console.log(persons)
+    const newEntry = {
+      name: newName,
+    }
+    setPersons(persons.concat(newEntry))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+      <div>debug: {newName}</div>
+    </div>
+  )
+
+}
+
+export default App
