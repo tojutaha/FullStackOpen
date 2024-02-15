7c89d57f3555bd9e529a735dbb1faecd44025284
Tomi Halko <tomi.halko@gmail.com>
2024-02-15 23:55:01 +0200

    Osan 4, tehtävät 4.15-4.19

--- osa4/blogilista/backend/controllers/blogs.js
+++ osa4/blogilista/backend/controllers/blogs.js
@@ -1,8 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -12,8 +22,26 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json(request.body)
   }
 
-  const blog = new Blog({ title, author, url, likes })
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  //const user = await User.findById(request.body.userId)
+  const user = await User.findById(decodedToken.id)
+  if(!user) {
+    return response.status(400).json({ error: 'could not find user with give id' })
+  }
+
+  const blog = new Blog({
+    title: title, 
+    author: author, 
+    url: url, 
+    likes: likes, 
+    user: user._id })
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
   response.status(201).json(savedBlog)
 })
