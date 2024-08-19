68198c22e49c52030aa368fea890f199d726e9d2
Tomi Halko <tomi.halko@gmail.com>
2024-08-19 23:29:27 +0300

    Osan 5, tehtävä 5.14

--- osa5/blogilista_osa5/frontend/src/components/Blog.test.js
+++ osa5/blogilista_osa5/frontend/src/components/Blog.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import '@testing-library/jest-dom'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Blog from './Blog';
 
-test('renders content', () => {
-
+test('renders title', () => {
   const blog = {
     title: 'testTitle',
     author: 'author',
@@ -17,39 +16,47 @@ test('renders content', () => {
   const deleteBlog = jest.fn()
 
   render(<Blog updateBlog={updateBlog} deleteBlog={deleteBlog} blog = {blog} />)
-
-  expect(screen.queryByText('testTitle')).toBeNull()
-
-  const viewButton = screen.getByText('View')
-  fireEvent.click(viewButton)
-  console.log(screen.debug())
+  // screen.debug()
 
   const element = screen.getByText(/testTitle/i)
   expect(element).toBeInTheDocument()
 })
-/*
-test('renders content', async () => {
+
+test('renders title, author, url, likes and user when View button is pressed', () => {
 
   const blog = {
     title: 'testTitle',
     author: 'author',
     url: 'url',
     likes: 42,
-    user: 'test'
+    user: 'testUser'
   }
 
   const updateBlog = jest.fn()
   const deleteBlog = jest.fn()
 
   render(<Blog updateBlog={updateBlog} deleteBlog={deleteBlog} blog = {blog} />)
-  console.log(screen.debug())
 
   expect(screen.queryByText('testTitle')).toBeNull()
 
   const viewButton = screen.getByText('View')
   fireEvent.click(viewButton)
+  // screen.debug()
 
-  const element = await waitFor(() => screen.getByText('testTitle'))
-  expect(element).toBeInTheDocument()
+  const titleElement = screen.getByText(/testTitle/i)
+  expect(titleElement).toBeInTheDocument()
+
+  const authorElement = screen.getByText(/author/i)
+  expect(authorElement).toBeInTheDocument()
+
+  const urlElement = screen.getByText(/url/i)
+  expect(urlElement).toBeInTheDocument()
+
+  const likesElement = screen.getByText(/42/i)
+  expect(likesElement).toBeInTheDocument()
+
+  // TODO: We dont have this yet in the Blog
+  // const userElement = screen.getByText(/testUser/i)
+  // expect(userElement).toBeInTheDocument()
 })
-*/
+
