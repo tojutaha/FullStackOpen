05fd2d350921d74ff3148f78300e34c85e0ead16
Tomi Halko <tomi.halko@gmail.com>
2024-10-08 00:25:03 +0300

    Osan 6, tehtävä 6.9

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,8 @@ import { useSelector, useDispatch } from 'react-redux'
 
 const AnecdoteList = () => {
 
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -12,7 +13,14 @@ const AnecdoteList = () => {
     })
   }
 
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+  const filteredAnecdotes = filter 
+    ? anecdotes.filter(anecdote =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase())
+      )
+    : anecdotes
+
+  const sortedAnecdotes = [...filteredAnecdotes].sort((a, b) => b.votes - a.votes)
+
   return (
       sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
--- osa6/redux-notes/src/index.js
+++ osa6/redux-notes/src/index.js
@@ -1,14 +1,17 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
 
-import anecdoteReducer from './reducers/anecdoteReducer'
+import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
 
 const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
+  notes: noteReducer,
+  filter: filterReducer
 })
 
 const store = createStore(reducer)
