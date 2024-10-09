2f00016ca73611f041131d0c4a7c20df9834d54a
Tomi Halko <tomi.halko@gmail.com>
2024-10-09 11:19:48 +0300

    Osan 6, tehtävä 6.10

--- osa6/redux-notes/src/reducers/filterReducer.js
+++ osa6/redux-notes/src/reducers/filterReducer.js
@@ -1,5 +1,5 @@
 const filterReducer = (state = 'ALL', action) => {
-  console.log("ACTION: ", action);
+  // console.log("ACTION: ", action);
   switch (action.type) {
     case 'SET_FILTER':
       return action.payload
