d0defed6e81e368b494fab28bf1ae414cd37b3a7
Tomi Halko <tomi.halko@gmail.com>
2024-10-14 21:47:29 +0300

    Osan 7, React-Bootstrap

--- osa7/router_app/src/main.jsx
+++ osa7/router_app/src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
+import { Table, Form, Button, Alert, Navbar, Nav } from 'react-bootstrap'
 
 import {
   BrowserRouter as Router,
@@ -33,13 +34,22 @@ const Note = ({ note }) => {
 const Notes = ({ notes }) => (
   <div>
     <h2>Notes</h2>
-    <ul>
-      {notes.map(note =>
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      )}
-    </ul>
+    <Table striped>
+      <tbody>
+        {notes.map(note =>
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>
+                {note.content}
+              </Link>
+            </td>
+            <td>
+              {note.user}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </div>
 )
 
@@ -66,15 +76,24 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>password:</Form.Label>
+          <Form.Control
+            type="password"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          login
+        </Button>
+      </Form>
     </div>
   )
 }
@@ -105,9 +124,14 @@ const App = () => {
   const note = match ? notes.find(note => note.id === Number(match.params.id)) : null
 
   const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
 
   const login = (user) => {
     setUser(user)
+    setMessage(`welcome ${user}`)
+    setTimeout(() => {
+      setUser(null)
+    }, 1000)
   }
 
   const padding = {
@@ -115,19 +139,37 @@ const App = () => {
   }
 
   return (
-    <div>
-        <div>
-          <Link style={padding} to="/">home</Link>
-          <Link style={padding} to="/notes">notes</Link>
-          <Link style={padding} to="/users">users</Link>
-          {user
-            ? <em>{user} logged in</em>
-            : <Link style={padding} to="/login">login</Link>
-          }
-        </div>
+    <div className='container'>
+      {(message &&
+        <Alert variant='success'>
+          {message}
+        </Alert>
+      )}
 
-        <Routes>
-          {/* <Route path="/notes/:id" element={<Note notes={notes} />} /> */}
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/">home</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/notes">notes</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/users">users</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              {user
+                ? <em>{user} logged in</em>
+                : <Link to="/login">login</Link>
+              }
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Routes>
           <Route path="/notes/:id" element={<Note note={note} />} />
           <Route path="/notes" element={<Notes notes={notes} />} />
           <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
