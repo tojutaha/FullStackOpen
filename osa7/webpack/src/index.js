e8ad97b65117ea67b174c61d4ce68657528277cc
Tomi Halko <tomi.halko@gmail.com>
2024-10-14 23:14:10 +0300

    Osan 7, Webpack

---
+++ osa7/webpack/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+import './index.css'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
