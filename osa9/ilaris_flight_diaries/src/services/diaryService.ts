681ba66d7e6a443f4ea718bb839c8fe1be5a7abd
Tomi Halko <tomi.halko@gmail.com>
2024-10-24 00:25:37 +0300

    Diary osa 9c

--- osa9/ilaris_flight_diaries/src/services/diaryService.ts
+++ osa9/ilaris_flight_diaries/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaries from '../../data/entries';
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import { NonSensitiveDiaryEntry, DiaryEntry, NewDiaryEntry } from '../types';
 
 const getEntries = (): DiaryEntry[] => {
   return diaries;
@@ -14,12 +14,25 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }));
 };
 
-const addDiary = () => {
-  return null;
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find(d => d.id === id);
+  return entry;
+};
+
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry,
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 export default {
   getEntries,
+  getNonSensitiveEntries,
+  findById,
   addDiary,
-  getNonSensitiveEntries
 };
