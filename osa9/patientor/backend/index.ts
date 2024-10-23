8d61144425c11cc93c70af380de13d11d2e5fb63
Tomi Halko <tomi.halko@gmail.com>
2024-10-23 19:02:10 +0300

    Osan 9, tehtävä 9.10

--- osa9/patientor/backend/index.ts
+++ osa9/patientor/backend/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import diagnosesRouter from './src/routes/diagnosesRouter';
+
 const app = express();
 app.use(express.json());
 
@@ -8,6 +10,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
