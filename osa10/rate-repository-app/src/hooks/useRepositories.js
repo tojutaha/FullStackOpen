d624f72893a86ef09d8bbb405fc85ebd8bffa874
Tomi Halko <tomi.halko@gmail.com>
2024-10-31 23:02:07 +0200

    Osan 10, tehtävä 10.11

---
+++ osa10/rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const [repositories, setRepositories] = useState();
+
+  const fetchRepositories = async () => {
+    if(data && data.repositories ) {
+      console.log(data);
+      setRepositories(data.repositories);
+    }
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, [data]);
+
+  if(error) {
+    console.error(error);
+  }
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
---
+++ osa10/rate-repository-app/src/utils/apolloClient.js
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    uri: 'http://127.0.0.1:4000/graphql',
+    cache: new InMemoryCache(),
+  });
+};
+
+export default createApolloClient;
